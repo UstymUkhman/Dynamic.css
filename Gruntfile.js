0ca824c44e4f909c0134367700950c9a070212c6
Ustym Ukhman <ustym.ukhman@gmail.com>
2016-01-05 00:51:12 +0100

    rollers initialized

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  var mainDesc =
+    '/*!\n' +
+    ' * Move.css - Library of CSS3 animations\n' +
+    ' *\n' +
+    ' * @version v<%= pkg.version %>\n' +
+    ' * @link <%= pkg.homepage %>\n' +
+    ' * @author Ustym Ukhman <ustym.ukhman@gmail.com>\n' +
+    ' * @license <%= pkg.license.type %> License, <%= pkg.license.url %>\n' +
+    ' *\n' +
+    ' * Date: <%= grunt.template.today("dd-mm-yyyy h:MM:ss TT Z") %>\n' +
+    ' */\n\n';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: '\n\n',
+        banner: mainDesc
+      },
+
+      dist: {
+        src: [
+          'css/main.css',
+          'css/rollers/*.css'
+        ],
+        dest: '<%= pkg.name %>.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat']);
+};
