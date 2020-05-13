1b41bfc3c900bfef8bbc35cde785567f65978629
Ustym <ustym.ukhman@gmail.com>
2020-05-13 14:35:57 +0300

    Demo page updated

--- public/index.js
+++ public/index.js
@@ -1,6 +1,31 @@
 'use strict';
 
 (function () {
+  var element;
+
+  function onButtonClick (event) {
+    var animation = event.target.dataset.animation;
+    element.className = 'dynamic ' + animation;
+  }
+
+  function onAnimationEnd () {
+    setTimeout(() => {
+      element.className = 'dynamic';
+    }, 250);
+  }
+
   window.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.getElementsByClassName('animation-button');
+        element = document.getElementById('animated-element');
+
+    for (var b = 0; b < buttons.length; b++) {
+      buttons[b].addEventListener('click', onButtonClick);
+    }
+
+    element.addEventListener('webkitAnimationEnd', onAnimationEnd);
+    element.addEventListener('mozAnimationEnd', onAnimationEnd);
+    element.addEventListener('MSAnimationEnd', onAnimationEnd);
+    element.addEventListener('oanimationend', onAnimationEnd);
+    element.addEventListener('animationend', onAnimationEnd);
   });
 })();
